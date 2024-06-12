ad53565d59bdd553cab83af6dfc6c672c5308a83
anoushka1111 <saha.anoushka@gmail.com>
2024-06-11 20:17:30 -0400

    turtle moves and game stops when turtle hits car

--- car_manager.py
+++ car_manager.py
@@ -1,23 +1,30 @@
-#Anoushka Saha
-#Turtle Crossing - Car Class
-#100 Days of Code - Day 23
-#May 26th, 2024
+# Anoushka Saha
+# Turtle Crossing - Car Class
+# 100 Days of Code - Day 23
+# May 26th, 2024
+
 from turtle import Turtle
 import random
 
-COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
-STARTING_MOVE_DISTANCE = 5
-MOVE_INCREMENT = 10
-
-
 class CarManager:
     def __init__(self):
-        cars = []
+        self.all_cars = []
+        self.car_speed = 5
 
     def create_cars(self):
-        new_car = Turtle("square")
-        new_car.shapesize(stretch_wid=2, stretch_len=1)
-        new_car.penup()
-        new_car.colour(random.choice(COLORS))
-        random_y = random.randint(-250,250)
-        new_car.goto(300, random_y)
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(["red", "blue", "green", "yellow", "purple", "orange"]))
+            random_y = random.randint(-250, 250)
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += 10
--- main.py
+++ main.py
@@ -16,6 +16,9 @@ screen.tracer(0)
 #Creating new player
 player = Player()
 
+#Car manager object
+car_manager = CarManager()
+
 #Move the player up when the "Up" key is pressed
 screen.listen()
 screen.onkey(player.move, "Up")
@@ -24,3 +27,13 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    #Collision detection
+    for car in car_manager.all_cars:
+       if car.distance(player) < 20:
+           game_is_on = False 
+
+screen.exitonclick
